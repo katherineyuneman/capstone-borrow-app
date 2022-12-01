9c714d010f71cfdeda92f7679de82bde93d9a4eb
katherineyuneman <kyuneman@gmail.com>
2022-11-30 21:11:29 -0500

    Removes incorrect issue from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,20 @@ Rails.application.routes.draw do
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  Rails.application.routes.draw do
-    namespace :api do
-      
-      post '/signup', to: 'users#create'
-      get '/me', to: 'users#show'
-      post '/login', to: 'sessions#create'
-      delete '/logout', to: 'sessions#destroy'
-    
-      resources :titles, only: [:index, :show]
-    
-      resources :authors, only: [:index, :show] do
-        resources :titles, only: [:index]
-      end
-    
-      resources :rentals
-      resources :book_rentals, only: [:destroy]
-    
-      post '/titles_books', to: 'titles#titles_books'
-    end
-  
-    get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  resources :titles, only: [:index, :show]
+
+  resources :authors, only: [:index, :show] do
+    resources :titles, only: [:index]
   end
+
+  resources :rentals
+  resources :book_rentals, only: [:destroy]
+
+  post '/titles_books', to: 'titles#titles_books'
 
 end
