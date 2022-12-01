926f6c7b22bc9248a86f93d890091b8e984ec9b8
katherineyuneman <kyuneman@gmail.com>
2022-11-30 20:49:29 -0500

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,30 +4,25 @@ Rails.application.routes.draw do
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  resources :titles, only: [:index, :show]
-
-  resources :authors, only: [:index, :show] do
-    resources :titles, only: [:index]
-  end
-
-  resources :rentals
-  resources :book_rentals, only: [:destroy]
-
-  post '/titles_books', to: 'titles#titles_books'
 
   Rails.application.routes.draw do
     namespace :api do
-      resources :recipes, only: [:index, :create]
-      post "/signup", to: "users#create"
-      get "/me", to: "users#show"
-      post "/login", to: "sessions#create"
-      delete "/logout", to: "sessions#destroy"
-    end
+      post '/signup', to: 'users#create'
+      get '/me', to: 'users#show'
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+
+      resources :titles, only: [:index, :show]
+
+      resources :authors, only: [:index, :show] do
+        resources :titles, only: [:index]
+       end
+
+      resources :rentals
+      resources :book_rentals, only: [:destroy]
+
+      post '/titles_books', to: 'titles#titles_books'
+      end
   
     get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   end
