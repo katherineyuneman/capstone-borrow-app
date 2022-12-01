b605dea98213138068efc1485b16b228a5598124
katherineyuneman <kyuneman@gmail.com>
2022-11-30 21:07:31 -0500

    Cleans up puma.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,27 @@ Rails.application.routes.draw do
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
+  
 
   Rails.application.routes.draw do
     namespace :api do
+      
       post '/signup', to: 'users#create'
       get '/me', to: 'users#show'
       post '/login', to: 'sessions#create'
       delete '/logout', to: 'sessions#destroy'
-
+    
       resources :titles, only: [:index, :show]
-
+    
       resources :authors, only: [:index, :show] do
         resources :titles, only: [:index]
-       end
-
+      end
+    
       resources :rentals
       resources :book_rentals, only: [:destroy]
-
+    
       post '/titles_books', to: 'titles#titles_books'
-      end
+    end
   
     get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   end
