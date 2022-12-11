04647a70c68a1c49be3a081cbdc9bf01f15568e6
katherineyuneman <kyuneman@gmail.com>
2022-12-11 16:21:05 -0500

    Adds AuthorForm functionality to add new author.

---
+++ client/src/components/forms/AuthorForm.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+const AuthorForm = () => {
+    const [ authorInputs, setAuthorInputs ] = useState([{
+        first_name:"",
+        last_name:""
+    }])
+
+    const handleAuthorInputs = (e) => {
+        setAuthorInputs({
+            ...authorInputs,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleAuthorSubmit = () => {
+        
+    }
+
+  return (
+    <div>
+      <form onSubmit={(e) => handleAuthorSubmit(e, authorInputs)}>
+        <label>First Name: 
+            <input type="text" name="first_name" value={authorInputs.first_name} onChange={handleAuthorInputs}/>
+        </label>
+        <label>Last Name: 
+            <input type="text" name="last_name" value={authorInputs.last_name} onChange={handleAuthorInputs}/>
+        </label>
+        <br />
+        <button>Submit Author</button>
+      </form>
+    </div>
+  )
+}
+
+export default AuthorForm
--- client/src/components/forms/TitleForm.js
+++ client/src/components/forms/TitleForm.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import AuthorForm from './AuthorForm'
 
 const TitleForm = () => {
     const [ titleInputs, setTitleInputs ] = useState([{
@@ -6,9 +7,17 @@ const TitleForm = () => {
         rating:"",
         genre:"",
         publication_date:"",
-        image_url:""
+        image_url:"",
+        author_id:""
+    }])
+
+    const [ authorInputs, setAuthorInputs ] = useState([{
+        first_name: "",
+        last_name: "",
+        
     }])
     const [ authors, setAuthors ] = useState([])
+    const [ displayNewAuthorForm, setDisplayNewAuthorForm ] = useState(false)
 
     useEffect(() => {
         fetch ('/authors')
@@ -32,15 +41,29 @@ const TitleForm = () => {
         })
     }
 
+    const handleNewAuthorInput = (e) => {
+       if (e.target.value === 'addNew'){
+        setDisplayNewAuthorForm(true)
+       } else setDisplayNewAuthorForm(false)
+    }
     
 
   return (
     <div>
       <h3>Add a new title and author below:</h3>
       <form>
-        <label>Title:
+        <label>Title: 
               <input type="text" name="title" value={titleInputs.title} maxLength={30} onChange={handleTitleInputs}/>
         </label>
+        <br/>
+        <lable>Main Author: 
+        <select name="author_id" value={titleInputs.author_id} required onChange={(e) => handleNewAuthorInput(e)}>
+            <option name="default" value="default">Select Author</option>
+            <option name="addNew" value="addNew">ADD NEW AUTHOR</option>
+            {authorDropDownOptions}
+        </select>
+        </lable>
+        {displayNewAuthorForm ? <AuthorForm /> : null}
         
       </form>
     </div>
